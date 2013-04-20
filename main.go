c26ca9c368c1cbeb8451d8e1f1c4d0068b1a5296
Yannik Dällenbach <yannikdbach@gmail.com>
2013-04-20 23:45:41 +0200

    added websocket handler and code handler

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import(
 	"net/http"
 	"os"
 	"darwin/web"
+	ws "code.google.com/p/go.net/websocket"
 )
 
 func main(){
@@ -11,6 +12,9 @@ func main(){
 	//This is just a test
 	http.HandleFunc("/", web.IndexHandler)
 	http.HandleFunc("/mobile", web.MobileIndexHandler)
+	http.Handle("/wsb", ws.Handler(web.BrowserSocketHandler))
+	http.HandleFunc("/register", web.RegisterMobileHandler)
+	http.Handle("/wsm", ws.Handler(web.MobileSocketHandler))
 
 	err:=http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 	if err != nil{
