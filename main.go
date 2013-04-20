10b959d09c98081650ed13d49f7e50f0f46ea7f6
Yannik Dällenbach <yannikdbach@gmail.com>
2013-04-20 17:57:47 +0200

    use index handler from web package

--- main.go
+++ main.go
@@ -3,15 +3,13 @@ package main
 import(
 	"net/http"
 	"os"
+	"darwin/web"
 )
 
 func main(){
 	//This is just a test
 	//This is just a test
-	http.HandleFunc("/", func(r http.ResponseWriter, req *http.Request){
-		r.Write([]byte("Hello World"))
-		return
-	})
+	http.HandleFunc("/", web.IndexHandler)
 
 	err:=http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 	if err != nil{
