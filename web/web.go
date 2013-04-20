a58ca154fb8117e7cfd3def2c2905120742de975
Yannik Dällenbach <yannikdbach@gmail.com>
2013-04-20 20:00:15 +0200

    generate unix-timestamp for id

--- web/web.go
+++ web/web.go
@@ -7,19 +7,25 @@ import(
 	"os"
 	"net/http"
 	"html/template"
+	"time"
 )
 
 var index *template.Template
 
 func init(){
-	log.Println(os.Getenv("TEMPLATE"))
+	log.Println("Template-Directory: "+os.Getenv("TEMPLATE"))
 	path := os.Getenv("TEMPLATE")
 	index = template.Must(template.ParseFiles(path+"index.html"))
 }
 
+//Struct for the index template
+type indexPage struct{
+	Code int64
+}
+
 func IndexHandler(w http.ResponseWriter, r *http.Request){
+	unix := time.Now().Unix()
 	w.Header().Set("content-type", "text/html")
-	index.Execute(w, nil)
-	return
+	index.Execute(w, indexPage{unix})
 }
 
