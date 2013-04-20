ad897d568b5e1622cbebe89353b5d563c54a167a
Yannik Dällenbach <yannikdbach@gmail.com>
2013-04-20 17:57:05 +0200

    added index.html and load as template

--- web/web.go
+++ web/web.go
@@ -2,5 +2,24 @@
 //templates and structs
 package web
 
+import(
+	"log"
+	"os"
+	"net/http"
+	"html/template"
+)
 
+var index *template.Template
+
+func init(){
+	log.Println(os.Getenv("TEMPLATE"))
+	path := os.Getenv("TEMPLATE")
+	index = template.Must(template.ParseFiles(path+"index.html"))
+}
+
+func IndexHandler(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("content-type", "text/html")
+	index.Execute(w, nil)
+	return
+}
 
