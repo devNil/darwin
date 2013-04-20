53da7b5ebde13df4ec02aa304100d49f1f4af32b
Yannik Dällenbach <yannikdbach@gmail.com>
2013-04-20 21:00:08 +0200

    mobile index handler is now used

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ func main(){
 	//This is just a test
 	//This is just a test
 	http.HandleFunc("/", web.IndexHandler)
+	http.HandleFunc("/mobile", web.MobileIndexHandler)
 
 	err:=http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 	if err != nil{
