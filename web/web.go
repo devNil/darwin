6505df9bf06fbd524050b7a9388ba5d745babac7
Yannik Dällenbach <yannikdbach@gmail.com>
2013-04-20 23:44:19 +0200

    added code handler and mobile controller template

--- web/web.go
+++ web/web.go
@@ -7,33 +7,48 @@ import(
 	"os"
 	"net/http"
 	"html/template"
-	"time"
 )
 
 var index *template.Template
 var mobileIndex *template.Template
+var controller *template.Template
 
 func init(){
 	log.Println("Template-Directory: "+os.Getenv("TEMPLATE"))
 	path := os.Getenv("TEMPLATE")
 	index = template.Must(template.ParseFiles(path+"index.html"))
 	mobileIndex = template.Must(template.ParseFiles(path+"mobile.html"))
+	controller = template.Must(template.ParseFiles(path+"controller.html"))
 }
 
 //Struct for the index template
 type indexPage struct{
-	Code int64
+	Url string
+}
+
+type controllerPage struct{
+	Url string
+	Id string
 }
 
 //Handler for the index-Page
 func IndexHandler(w http.ResponseWriter, r *http.Request){
-	unix := time.Now().Unix()
 	w.Header().Set("content-type", "text/html")
-	index.Execute(w, indexPage{unix})
+	index.Execute(w, indexPage{r.Host})
 }
 
 //Handler for the mobile-index-page
 func MobileIndexHandler(w http.ResponseWriter, r *http.Request){
 	w.Header().Set("content-type", "text/html")
 	mobileIndex.Execute(w, nil)
+}
+
+func RegisterMobileHandler(w http.ResponseWriter, r *http.Request){
+	r.ParseForm();
+
+	id := r.FormValue("id")
+
+	w.Header().Set("content-type", "text/html")
+
+	controller.Execute(w, controllerPage{r.Host, id})
 }
