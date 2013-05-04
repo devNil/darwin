d574d657c13fe50ca708d6d99059169497c667ea
Yannik Dällenbach <yannikdbach@gmail.com>
2013-05-04 21:19:18 +0200

    simple

--- socket/game.go
+++ socket/game.go
@@ -1,3 +1,34 @@
 package socket
 
+import(
+	"fmt"
+	ws "code.google.com/p/go.net/websocket"
+)
+
+var colors = {0xFF00FF, 0xFFFF00}
+
+type entity struct{
+	id,X,Y,Dir int8
+	Color string
+	S int8
+}
+
+type game struct{
+	idc int8
+	entities []*entity
+	board [(640/16)*(480/8)]int32
+}
+
+func Run(){
+}
+
+type command struct{
+	Id int8
+	Value []byte
+}
+
+type client struct{
+	conn *ws.Conn
+	e *entity
+}
 
